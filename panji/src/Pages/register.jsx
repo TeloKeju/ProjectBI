6f643614927b5feb2c7d8432b10aa127e2414372
Dani Ardiansyach <107809518+TeloKeju@users.noreply.github.com>
2024-07-23 22:44:36 +0700

    Update register.jsx

--- panji/src/Pages/register.jsx
+++ panji/src/Pages/register.jsx
@@ -46,35 +46,99 @@ const register = () => {
     }
 
     return (
-        <>
-            <Header></Header>
-            <label htmlFor="">{namaMessage}</label>
-            <input type="text" value={nama} onChange={(e) => {
-                setNama(e.target.value)
-                setNamaMessage("")
-            }} />
-
-            <label htmlFor="">{emailMessage}</label>
-            <input type="email" value={email} onChange={(e) => {
-                setEmail(e.target.value)
-                setEmailMessage("")
-            }} />
-
-            <label htmlFor="">{passwordMessage}</label>
-            <input type="password" value={password} onChange={(e) => {
-                setPassword(e.target.value)
-                setPasswordMessage('')
-            }} />
-
-            <label htmlFor="">{confirmPasswordMessage}</label>
-            <input type="password" value={confirmPassword} onChange={(e) => {
-                setConfirmPassword(e.target.value)
-                setConfirmPasswordMessage("")
-            }} />
-            <button type="submit" onClick={submit}>Daftar</button>
-            <Footer></Footer>
-        </>)
-
+    <>
+      <Header></Header>
+      <section className="row justify-content-center pt-2 pt-md-5 p-3 p-md-0 register">
+        <section className="col-lg-5">
+          <h2 className="text-blue mt-4 mt-md-0">Registrasi</h2>
+          <section className="card mt-2 shadow">
+            <section className="card-body p-4">
+              <form>
+                <section className="form-group mb-3">
+                  <label htmlFor="" className="form-label fw-bold">
+                    {namaMessage}NAMA LENGKAP
+                  </label>
+                  <input
+                    type="text"
+                    value={nama}
+                    onChange={(e) => {
+                      setNama(e.target.value);
+                      setNamaMessage("");
+                    }}
+                    className="form-control"
+                    placeholder="Masukan nama lengkap"
+                  />
+                </section>
+                <section className="form-group mb-3">
+                  <label htmlFor="" className="form-label fw-bold">
+                    {emailMessage}ALAMAT EMAIL
+                  </label>
+                  <input
+                    type="text"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                      setEmailMessage("");
+                    }}
+                    className="form-control"
+                    placeholder="Masukan alamat email"
+                  />
+                </section>
+                <section className="form-group mb-3">
+                  <label htmlFor="" className="form-label fw-bold">
+                    {passwordMessage}KATA SANDI
+                  </label>
+                  <input
+                    type="text"
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                      setPasswordMessage("");
+                    }}
+                    className="form-control"
+                    placeholder="Masukan kata sandi"
+                  />
+                </section>
+                <section className="form-group mb-3">
+                  <label htmlFor="" className="form-label fw-bold">
+                    {passwordMessage}ULANGI KATA SANDI
+                  </label>
+                  <input
+                    type="text"
+                    value={confirmPassword}
+                    onChange={(e) => {
+                      setConfirmPassword(e.target.value);
+                      setConfirmPasswordMessage("");
+                    }}
+                    className="form-control"
+                    placeholder="Masukan ulang kata sandi"
+                  />
+                </section>
+                <section className="form-group d-grid gap-2">
+                  <button
+                    type="submit"
+                    onClick={submit}
+                    className="btn btn-orange py-2 text-white"
+                  >
+                    Daftar
+                  </button>
+                </section>
+              </form>
+              <section className="form-group text-center mt-4">
+                <p>
+                  Sudah punya akun?
+                  <a href="/login" className="text-decoration-none text-blue">
+                    Masuk disini
+                  </a>
+                </p>
+              </section>
+            </section>
+          </section>
+        </section>
+      </section>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default register;
