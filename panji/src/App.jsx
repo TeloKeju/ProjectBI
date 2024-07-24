4e1b4db68b2ab16911a20fb18e5fc23d55659e2c
alungggggg <aldinoalungputraanugraha@gmail.com>
2024-07-24 15:37:44 +0700

    p

--- panji/src/App.jsx
+++ panji/src/App.jsx
@@ -1,22 +1,7 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Routers from "./route";
-// import Home from "./Pages/Services/home";
-// import Katalog from "./Pages/Services/katalog";
-// import Petunjuk from "./Pages/Services/petunjuk";
-// import Detail from "./Pages/Services/detail";
-// import DongengView from "./Pages/Services/dongengView"
 
-// import Login from "./Pages/Authentication/login"
-// import Register from "./Pages/Authentication/register"
-// import Logout from "./Pages/Authentication/logout"
-
-// import User from "./Pages/Admin/user"
-// import UpdateUser from "./Pages/Admin/updateUser"
-// import AddUser from "./Pages/Admin/addUser"
-
-// import Dongeng from "./Pages/Admin/dongeng"
-// import UpdateDongeng from "./Pages/Admin/updateDongeng"
 
 function App() {
 
